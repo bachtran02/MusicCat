0527eed79942a751f0a062ba72584766b1da0c87
Peter Tran <xuanbachtran02@gmail.com>
2023-03-18 13:10:03 -0700

    move base youtube url to here

--- bot/utils.py
+++ bot/utils.py
@@ -1,12 +1,5 @@
 import urllib.parse
 
-COLOR_DICT = {
-    'RED': 0xd25557,
-    'BLUE': 0x00E7FF ,
-    'YELLOW': 0xf9c62b,
-    'GREEN': 0x76ffa1,
-}
-
 def get_spotify_playlist_id(url: str):
 
     parsed_url = urllib.parse.urlparse(url)
@@ -23,3 +16,12 @@ def ms_to_minsec(ms: int) -> str:
 
     length = divmod(ms, 60000)
     return f'{int(length[0])}:{round(length[1]/1000):02}'
+
+COLOR_DICT = {
+    'RED': 0xd25557,
+    'BLUE': 0x00E7FF ,
+    'YELLOW': 0xf9c62b,
+    'GREEN': 0x76ffa1,
+}
+
+BASE_YT_URL = 'https://www.youtube.com/watch'
