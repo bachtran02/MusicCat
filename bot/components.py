4717f531eba0651a7c3d149adc9e8ea732c51988
Peter Tran <xuanbachtran02@gmail.com>
2023-04-18 12:08:34 -0700

    use components for interactive player

---
+++ bot/components.py
@@ -0,0 +1,108 @@
+import typing as t
+import lightbulb
+import hikari
+from hikari.impl import MessageActionRowBuilder
+
+from bot.constants import COLOR_DICT
+from bot.utils import player_bar
+
+PLAYER_BUTTONS = [
+    {
+        'id': 'previous',
+        'emoji': '⏮️',
+    },
+    {
+        'id': 'playpause',
+        'emoji': '⏯️',
+    },
+        {
+        'id': 'stop',
+        'emoji': '⏹️',
+    },
+    {
+        'id': 'next',
+        'emoji': '⏭️',
+    },
+]
+
+async def generate_rows(bot: lightbulb.BotApp) -> t.Iterable[MessageActionRowBuilder]:
+    """Generate 2 action rows with 4 buttons each."""
+
+    rows: t.List[MessageActionRowBuilder] = []
+
+    row = bot.rest.build_message_action_row()
+
+    for i, button in enumerate(PLAYER_BUTTONS):
+        if i % 4 == 0 and i != 0:
+            rows.append(row)
+            row = bot.rest.build_message_action_row()
+
+        # We use an enclosing scope here so that we can easily chain
+        # method calls of the action row.
+        (
+            row.add_interactive_button(
+                hikari.ButtonStyle.SECONDARY,
+                button['id'],
+                emoji=button['emoji'],
+            )
+        )
+
+    rows.append(row)
+    return rows
+
+
+async def handle_responses(
+    bot: lightbulb.BotApp, author: hikari.User, message: hikari.Message,
+) -> None:
+    """Watches for events, and handles responding to them."""
+
+    with bot.stream(hikari.InteractionCreateEvent, timeout=120).filter(
+        lambda e: (
+            isinstance(e.interaction, hikari.ComponentInteraction)  # a component interaction is a button interaction.
+            and e.interaction.user == author  # Make sure the command author hit the button.
+            and e.interaction.message == message  # Make sure the button was attached to our message.
+        )
+    ) as stream:
+        async for event in stream:
+            cid = event.interaction.custom_id
+            player = bot.d.lavalink.player_manager.get(event.interaction.guild_id)
+            
+            if cid == 'playpause':
+                await player.set_pause(not player.paused) 
+            elif cid == 'stop':
+                await player.stop()
+            elif cid == 'next': 
+                await player.skip()
+            elif cid == 'previous':
+                # play previous track
+                pass
+
+            if not player.is_playing:
+                embed = hikari.Embed(
+                    description='No track to play!',
+                    color=COLOR_DICT['YELLOW'],
+                )
+            else:
+                body = f'**Streaming:** [{player.current.title}]({player.current.uri})' + '\n'
+                body += player_bar(player)
+                body += f'Requested by: <@!{player.current.requester}>'
+                embed = hikari.Embed(
+                    description=body,
+                    color=COLOR_DICT['GREEN'],
+                )
+
+            # If we haven't responded to the interaction yet, we
+            # need to create the initial response. Otherwise, we
+            # need to edit the initial response.
+            try:
+                await event.interaction.create_initial_response(
+                    hikari.ResponseType.MESSAGE_UPDATE,
+                    embed=embed,
+                )
+            except hikari.NotFoundError:
+                await event.interaction.edit_initial_response(
+                    embed=embed,
+                )
+
+    # remove the buttons after timeout
+    await message.edit(components=[])
--- bot/utils.py
+++ bot/utils.py
@@ -1,4 +1,5 @@
 import urllib.parse
+import lavalink
 
 def get_spotify_playlist_id(url: str):
 
@@ -12,7 +13,7 @@ def get_spotify_playlist_id(url: str):
     path_parts = parsed_url.path.split("/")
     return (True, path_parts[-1])
 
-def convert_ms(ms) -> str:
+def convert_ms(ms: int) -> str:
 
     ms = round(ms)
     total_seconds = ms // 1000
@@ -23,10 +24,40 @@ def convert_ms(ms) -> str:
 
     return (hours, minutes, seconds)
 
-def duration_str(duration):
+def duration_str(duration: int) -> str:
     
     time = convert_ms(duration)
     timestr = f'{time[0]}:{time[1]:02}' if time[0] else f'{time[1]}'
     timestr += f':{time[2]:02}'
 
     return timestr
+
+def progress_bar(percent: float) -> str:
+
+    str = ''
+    for i in range(12):
+        if i == (int)(percent*12):
+            str+='🔘'
+        else:
+            str+='▬'
+    return str;
+
+def player_bar(player: lavalink.DefaultPlayer):
+
+    loop_emj = ''
+    if player.loop == player.LOOP_SINGLE:
+        loop_emj = '🔂 '
+    elif player.loop == player.LOOP_QUEUE:
+        loop_emj = '🔁 '
+
+    play_emj = '▶️' if not player.paused else '⏸️'
+    shuffle_emj = '🔀 ' if player.shuffle else ''
+
+    if player.current.stream:
+        playtime = 'LIVE'
+        bar = progress_bar(0.99)
+    else:
+        playtime = f'{duration_str(player.position)} | {duration_str(player.current.duration)}'
+        bar = progress_bar(player.position/player.current.duration)
+
+    return f'{play_emj} {bar} `{playtime}` {loop_emj}{shuffle_emj} \n'
