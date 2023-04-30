6923dc127ffd5a78fdfd8e8b0072c8ba28981b29
Peter Tran <xuanbachtran02@gmail.com>
2023-04-29 23:28:27 -0700

    update intents, add code to run miru

--- bot/bot.py
+++ bot/bot.py
@@ -2,14 +2,14 @@ import os
 import hikari
 from hikari import Intents
 import lightbulb
+import miru
 
 from bot.logger.bot_logger import bot_logging_config
 
 bot = lightbulb.BotApp(
     os.environ['TOKEN'],
-    intents=(Intents.GUILDS | Intents.GUILD_MESSAGES | Intents.GUILD_VOICE_STATES),
-    help_slash_command=True,
-    banner=None,
+    intents=(Intents.GUILDS | Intents.GUILD_VOICE_STATES),
+    help_slash_command=True, banner=None,
     logs=bot_logging_config,
 )
 
@@ -17,6 +17,7 @@ bot = lightbulb.BotApp(
 bot.load_extensions_from('./bot/extensions', must_exist=True)
 
 def run() -> None:
+    miru.install(bot) # Load miru and attach it to the bot instance.
     bot.run()
 
 async def remove_command(guild_id=hikari.UNDEFINED):
@@ -24,6 +25,6 @@ async def remove_command(guild_id=hikari.UNDEFINED):
     rest = hikari.RESTApp()
     await rest.start()
     
-    client = rest.acquire(os.environ["TOKEN"], hikari.TokenType.BOT)
+    client = rest.acquire(os.environ['TOKEN'], hikari.TokenType.BOT)
     application = await client.fetch_application()
     await client.set_application_commands(application.id, (), guild=guild_id)
