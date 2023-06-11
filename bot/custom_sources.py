4f12d3404966d9a143329db52b2955eb109c9c8d
Peter Tran <xuanbachtran02@gmail.com>
2023-06-11 05:49:50 -0700

    change dir

--- bot/custom_sources.py
+++ bot/custom_sources.py
@@ -1,16 +1,12 @@
-from typing import Any, Optional
-from lavalink import (DeferredAudioTrack, LoadResult, LoadType, Source, Client, LoadResult)
+from lavalink import (LoadResult, LoadType, Source, LoadResult)
 import typing as t
 import random
 import requests
 from bot.constants import BASE_YT_URL
 
-# from bot.fixture import Fixture
-
 class LoadError(Exception):
     pass
 
-# source to search and access Spotify playlists only
 class lnchillSource(Source):
     def __init__(self, token: str = None):
         super().__init__(name='linhnhichill')
@@ -28,7 +24,7 @@ class lnchillSource(Source):
     async def load_item(self, client, query: str = None, use_source: bool = False) -> t.Optional[LoadResult]:
         
         # prevent source from being used out of use case
-        if not use_source:  
+        if not use_source:
             return None
         
         url = f'{BASE_YT_URL}/playlist?list=PL-F2EKRbzrNQte4aGjHp9cQau9peyPMw0'
@@ -49,49 +45,3 @@ class lnchillSource(Source):
             return LoadResult(LoadType.TRACK, tracks=[result.tracks[selected_track]], playlist_info=None)
         return LoadResult(LoadType.NO_MATCHES, tracks=[], playlist_info=None)
 
-
-# class talkSportStream(DeferredAudioTrack):
-
-#     def __init__(self, data: dict, requester: int = 0, stream_url : str = None, **extra):
-#         super().__init__(data, requester, **extra)
-#         self.stream_url = stream_url
-
-#     async def load(self, client):
-        
-#         # search for track using isrc
-#         result: LoadResult = await client.get_tracks(query=self.stream_url)
-#         if result.load_type != LoadType.TRACK or not result.tracks:
-#             raise LoadError
-
-#         first_track = result.tracks[0] 
-
-#         base64 = first_track.track  
-#         self.track = base64
-
-#         return base64
-
-
-# class talkSportSource(Source):
-#     def __init__(self):
-#         super().__init__(name='talkSport')
-    
-#     async def load_item(self, client, query: Fixture = None, use_source: bool = False) -> t.Optional[LoadResult]:
-
-#         if not use_source:
-#             return None
-        
-#         result = [talkSportStream(
-#             data={
-#                 'identifier': query.fid,
-#                 'isSeekable': False,
-#                 'author': 'talkSPORT',
-#                 'length': 0x7FFFFFFFFFFFFFFF,  # stream never expires
-#                 'isStream': True,
-#                 'title': query.title,
-#                 'uri': query.url,
-#                 'isrc': None,
-#             },
-#             requester=0,
-#             stream_url=query.stream_url
-#         )]
-#         return LoadResult(LoadType.TRACK, result, playlist_info=None)
