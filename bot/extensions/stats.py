66271af2d1a1266fdf4a06869360e1168d496067
bachtran <xuanbachtran02@gmail.com>
2023-07-20 17:31:08 +0700

    rework /stats

--- bot/extensions/stats.py
+++ bot/extensions/stats.py
@@ -7,35 +7,36 @@ from bot.constants import COLOR_DICT
 plugin = lightbulb.Plugin('Stats', 'Stats commands')
 
 @plugin.command()
-@lightbulb.add_checks(lightbulb.owner_only)
-@lightbulb.command('stats', 'Display bot stats.', auto_defer = True)
+# @lightbulb.add_checks(lightbulb.owner_only)
+@lightbulb.command('stats', 'Display bot stats.', auto_defer=True)
 @lightbulb.implements(lightbulb.SlashCommand)
 async def stats(ctx: lightbulb.Context) -> None:
     """Display bot stats."""
 
-    client = plugin.bot.d.lavalink
-    embed = hikari.Embed(
-        title='Bot Stats',
-        color= COLOR_DICT['BLUE'],
-    )
+    node_manager = plugin.bot.d.lavalink.node_manager
 
-    # node stats
-    node_manager = client.node_manager
-    embed.description = f'Nodes: {len(node_manager.nodes)}' 
+    stats = None
+    body = '**Nodes:**' + '\n'
     for i, node in enumerate(node_manager.nodes):
-        stats = node.stats
-        field_body = ''
-        field_body += f'Uptime: `{format_time(stats.uptime, "d")}`'+ '\n'
-        field_body += f'Players: `{stats.players} ({stats.playing_players} playing)`' + '\n'
-        field_body += f'Memory: `{round(stats.memory_used/1e6)} MB ({round(100*stats.memory_used/stats.memory_allocated)}%)`'  + '\n'
-        field_body += f'Lavalink load: `{round(stats.lavalink_load*100, 2)}%`' + '\n'
-        field_body += f'System load: `{round(stats.system_load*100, 2)}%`' + '\n'
-        field_body += f'Frames sent: `{stats.frames_sent}`' + '\n'
-        embed.add_field(
-            name=f'Node #{i + 1}:',
-            value=field_body
-        )
-    await ctx.respond(embed=embed)
+        body += f'{i+1}. `{node.name} [{node.region}]`' + '\n'
+        if not node.stats.is_fake:
+            stats = node.stats
+
+    body += '\n' + '**Lavalink Stats:**' + '\n'    
+    if stats:
+        body += f'Uptime: `{format_time(stats.uptime, "d")}`'+ '\n'
+        body += f'Players: `{stats.players} ({stats.playing_players} playing)`' + '\n'
+        body += f'Memory: `{round(stats.memory_used/1e6)} MB ({round(100*stats.memory_used/stats.memory_allocated)}%)`'  + '\n'
+        body += f'Lavalink load: `{round(stats.lavalink_load*100, 2)}%`' + '\n'
+        body += f'System load: `{round(stats.system_load*100, 2)}%`' + '\n'
+        body += f'Frames sent: `{stats.frames_sent}`' + '\n'
+    else:
+        body += 'No stats available!' + '\n'
+
+    await ctx.respond(embed=hikari.Embed(
+        title = '📊 Bot Stats',
+        description = body,
+        colour = COLOR_DICT['BLUE']))
 
 
 @plugin.set_error_handler
