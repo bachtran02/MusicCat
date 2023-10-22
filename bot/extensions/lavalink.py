8233cc7b452e69edbe4d1f76558f298d49c84e0b
Peter Tran <xuanbachtran02@gmail.com>
2023-10-22 11:07:36 -0700

    improve formatting

--- bot/extensions/lavalink.py
+++ bot/extensions/lavalink.py
@@ -17,16 +17,16 @@ async def stats(ctx: lightbulb.Context) -> None:
     node_manager = plugin.bot.d.lavalink.node_manager
     body = '**Nodes:**' + '\n'
     for i, node in enumerate(node_manager.nodes):
-        body += f'{i+1}. `{node.name} [{node.region}]`' + '\n'
+        body += '{}. `{} []`\n'.format(i + 1, node.name, node.region)
+    body += '\n**Lavalink Stats:**\n'
 
-    body += '\n' + '**Lavalink Stats:**' + '\n'
     if node_manager.nodes and (stats := node_manager.nodes[0].stats) and not stats.is_fake:
-        body += f'Uptime: `{format_time(stats.uptime, "d")}`'+ '\n'
-        body += f'Players: `{stats.players} ({stats.playing_players} playing)`' + '\n'
-        body += f'Memory: `{round(stats.memory_used/1e6)} MB ({round(100*stats.memory_used/stats.memory_allocated)}%)`'  + '\n'
-        body += f'Lavalink load: `{round(stats.lavalink_load*100, 2)}%`' + '\n'
-        body += f'System load: `{round(stats.system_load*100, 2)}%`' + '\n'
-        body += f'Frames sent: `{stats.frames_sent}`' + '\n'
+        body += 'Uptime: `{}`\nPlayers: `{} ({} playing)`\nMemory: `{} MB ({}%)`\n \
+            Lavalink load: `{}%`\nSystem load: `{}%`\nFrames sent: `{}`\n'.format(
+                format_time(stats.uptime, "d"), 
+                stats.players, stats.playing_players,
+                round(stats.memory_used/1e6), round(100*stats.memory_used/stats.memory_allocated),
+                round(stats.lavalink_load*100, 2), round(stats.system_load*100, 2), stats.frames_sent)
     else:
         body += 'No stats available' + '\n'
 
@@ -48,7 +48,7 @@ async def info(ctx: lightbulb.Context) -> None:
         body = ''
         info = await nodes[0].get_info()
         for item in info:
-            body += f'- {item}: `{info[item]}`' + '\n'
+            body += '- {}: `{}`\n'.format(item, info[item])
     else:
         body = 'No info available' + '\n' 
 
